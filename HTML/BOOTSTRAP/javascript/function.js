dcd4bee918b3da22e0024520bb560ab1d5848c9f
Annamalai1995 <s.annamalai1995@gmail.com>
2024-03-08 12:07:01 +0530

    jss

---
+++ HTML/BOOTSTRAP/javascript/function.js
@@ -0,0 +1,17 @@
+function data()
+{
+    alert("Happiness overloaded")
+
+}
+function datas1(name,college)
+{
+    alert("Name is"+name+":College is"+college)
+}
+var exe=()=>
+{
+    document.writeln(" Fat Arrow function")
+}
+const sum=(a,b)=>
+{
+return a+b;
+}
